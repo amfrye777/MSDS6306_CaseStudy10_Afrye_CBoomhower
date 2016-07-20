2aeb1ea814b3c43810e2d29f97c3c2e701d8956e
Amfrye777 <Amfrye@smu.edu>
2016-07-20 00:22:31 -0500

    EfakSmoothing Base Code Commit

---
+++ DataAnalysis/EfakSmoothing.R
@@ -0,0 +1,86 @@
+# ---- ExecAllSmooth ----
+
+  # Simple Exponential Smoothing
+Model_ses <- ses(EfakAsIs, h=12)
+
+  # Holt's Linear Trend
+Model_holt_1 <- holt(EfakAsIs,h=12)
+
+  # Holt's Exponential Trend
+Model_holt_2<- holt(EfakAsIs, exponential=TRUE,h=12)
+
+  # Holt's Damped Linear Trend
+Model_holt_3 <- holt(EfakAsIs, damped=TRUE,h=12)
+
+  # Holt's Damped Exponential Trend
+Model_holt_4 <- holt(EfakAsIs, exponential=TRUE, damped=TRUE,h=12)
+
+  # Holt Winter's Seasonal Additive Model
+Model_hw_1 <- hw(EfakAsIs ,seasonal="additive",h=12)
+
+  # Holt Winter's Seasonal Additive Model
+Model_hw_2 <- hw(EfakAsIs ,seasonal="multiplicative",h=12)
+
+
+  ## Compute Maximum Likelihood Estimation (AIC/AICc/BIC) Values for All Models
+
+
+# ---- SimpleExponential ----
+summary(Model_ses)
+plot(Model_ses, plot.conf=FALSE, ylab="Exports Chulwalar  )", xlab="Year", main="", fcol="white", type="o")
+lines(fitted(Model_ses), col="green", type="o")
+lines(Model_ses$mean, col="blue", type="o")
+legend("topleft",lty=1, col=c(1,"green"), c("data", expression(alpha == 0.671)),pch=1)
+
+# ---- HoltLinear ----
+summary(Model_holt_1)
+plot(Model_holt_1)
+
+# ---- HoltExponential ----
+summary(Model_holt_2)
+plot(Model_holt_2)
+
+# ---- HoltDampedLinear ----
+summary(Model_holt_3)
+plot(Model_holt_3)
+
+# ---- HoltDampedExponential ----
+summary(Model_holt_4)
+plot(Model_holt_4)
+
+# ---- HoltWinterSeasonalAdd ----
+summary(Model_hw_1)
+plot(Model_hw_1)
+
+# ---- HoltWinterSeasonalMult ----
+summary(Model_hw_2)
+plot(Model_hw_2)
+
+# ----
+
+# level and slope can be plotted individually for each model. 
+plot(Model_holt_1$model$state)
+plot(Model_holt_2$model$state)
+plot(Model_holt_3$model$state)
+plot(Model_holt_4$model$state)
+
+plot(Model_holt_1, plot.conf=FALSE, ylab="Exports Chulwalar  )", xlab="Year", main="", fcol="white", type="o")
+lines(fitted(Model_ses), col="purple", type="o")
+lines(fitted(Model_holt_1), col="blue", type="o")
+lines(fitted(Model_holt_2), col="red", type="o")
+lines(fitted(Model_holt_3), col="green", type="o")
+lines(fitted(Model_holt_4), col="orange", type="o")
+lines(Model_ses$mean, col="purple", type="o")
+lines(Model_holt_1$mean, col="blue", type="o")
+lines(Model_holt_2$mean, col="red", type="o")
+lines(Model_holt_3$mean, col="green", type="o")
+lines(Model_holt_4$mean, col="orange", type="o")
+legend("topleft",lty=1, col=c(1,"purple","blue","red","green","orange"), c("data", "SES","Holts auto", "Exponential", "Additive Damped", "Multiplicative Damped"),pch=1)
+
+plot(Model_hw_1, ylab="Exports Chulwalar  ", plot.conf=FALSE, type="o", fcol="white", xlab="Year")
+lines(fitted(Model_hw_1), col="red", lty=2)
+lines(fitted(Model_hw_2), col="green", lty=2)
+lines(Model_hw_1$mean, type="o", col="red")
+lines(Model_hw_2$mean, type="o", col="green")
+legend("topleft",lty=1, pch=1, col=1:3, c("data","Holt Winters' Additive","Holt Winters' Multiplicative"))
+
