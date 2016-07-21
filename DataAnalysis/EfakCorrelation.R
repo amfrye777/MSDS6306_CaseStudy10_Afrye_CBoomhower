ff11e89bb5a2fa21beecdee1e3d834067c97a32a
Chris Boomhower <cboomhower@smu.edu>
2016-07-21 01:52:45 -0500

    Draft writeup through correlation.

--- DataAnalysis/EfakCorrelation.R
+++ DataAnalysis/EfakCorrelation.R
@@ -5,11 +5,12 @@ plot(EfakAsIs , col="red",main="EfakAsIs")
 plot(EfakPlan , col="red",main="EfakPlan")
 
 # ---- EfakCor_Correlation ----
-cor(EfakAsIs , EfakPlan)
+cor(EfakAsIs , EfakPlan, method = "pearson")
 
-# ---- EfakCor_Accuracy ----
+# ----
 EfakAsIs_lm <- lm(EfakAsIs ~ EfakPlan , data = EfakAsIs)
 summary(EfakAsIs_lm)
 
+# ---- EfakCor_Accuracy ----
 EfakAsIs_tslm <- tslm(EfakAsIs ~ EfakPlan )
 summary(EfakAsIs_tslm)
