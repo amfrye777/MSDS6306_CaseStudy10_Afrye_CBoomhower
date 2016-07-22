bd902e847310be40b98cece9976ae1ba0ec04926
Amfrye777 <Amfrye@smu.edu>
2016-07-22 00:27:55 -0500

    PlotSES_HWSA

--- DataAnalysis/EfakSmoothing.R
+++ DataAnalysis/EfakSmoothing.R
@@ -126,6 +126,7 @@ theme(axis.text.x = element_text(angle = 90, hjust = 1))
 
 
 # ---- SimpleExponential ----
+par(mfrow=c(1,1))
 plot(Model_ses, plot.conf=FALSE, ylab="Exports Chulwalar  )", xlab="Year", main="", fcol="white", type="o")
 lines(fitted(Model_ses), col="green", type="o")
 lines(Model_ses$mean, col="blue", type="o")
@@ -149,31 +150,11 @@ plot(Model_hw_1)
 # ---- HoltWinterSeasonalMult ----
 plot(Model_hw_2)
 
-# ----
+# ---- PlotSES_HWSA ----
 
-# level and slope can be plotted individually for each model. 
-plot(Model_holt_1$model$state)
-plot(Model_holt_2$model$state)
-plot(Model_holt_3$model$state)
-plot(Model_holt_4$model$state)
-
-plot(Model_holt_1, plot.conf=FALSE, ylab="Exports Chulwalar  )", xlab="Year", main="", fcol="white", type="o")
+plot(Model_holt_1, plot.conf=FALSE, ylab="Exports Chulwalar", xlab="Year", main="", fcol="white", type="o")
 lines(fitted(Model_ses), col="purple", type="o")
-lines(fitted(Model_holt_1), col="blue", type="o")
-lines(fitted(Model_holt_2), col="red", type="o")
-lines(fitted(Model_holt_3), col="green", type="o")
-lines(fitted(Model_holt_4), col="orange", type="o")
-lines(Model_ses$mean, col="purple", type="o")
-lines(Model_holt_1$mean, col="blue", type="o")
-lines(Model_holt_2$mean, col="red", type="o")
-lines(Model_holt_3$mean, col="green", type="o")
-lines(Model_holt_4$mean, col="orange", type="o")
-legend("topleft",lty=1, col=c(1,"purple","blue","red","green","orange"), c("data", "SES","Holts auto", "Exponential", "Additive Damped", "Multiplicative Damped"),pch=1)
+lines(fitted(Model_hw_1), col="red", type="o")
+legend("topleft",lty=1, col=c(1,"purple","red"), c("data", "SES","Holt Winters' Additive"),pch=1)
 
-plot(Model_hw_1, ylab="Exports Chulwalar  ", plot.conf=FALSE, type="o", fcol="white", xlab="Year")
-lines(fitted(Model_hw_1), col="red", lty=2)
-lines(fitted(Model_hw_2), col="green", lty=2)
-lines(Model_hw_1$mean, type="o", col="red")
-lines(Model_hw_2$mean, type="o", col="green")
-legend("topleft",lty=1, pch=1, col=1:3, c("data","Holt Winters' Additive","Holt Winters' Multiplicative"))
 
